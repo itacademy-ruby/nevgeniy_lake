535f077a364a8fb4fb41a4c002201b2ce22aa5d4
root <root@vaverka-ubuntu.(none)>
2013-06-11 21:30:28 +0400

    Utochki-u

--- ducks_lake.rb
+++ ducks_lake.rb
@@ -18,7 +18,13 @@ module Swiming
     'Swiming'
   end
 end
- 
+
+module Eating
+  def eating
+    'Eating'
+  end
+end
+
 class Duck
   def fly
     "Can't Fly"
@@ -37,6 +43,7 @@ class RealDuck < Duck
 include Flying
 include Quacking
 include Swiming
+include Eating
 end
  
 class RubberDuck < Duck
@@ -47,16 +54,25 @@ end
 class WoodDuck < Duck
 include Swiming
 end
+
+class RobotsDuck < Duck
+include Flying
+include Quacking
+include Swiming
+include Eating
+end
  
  
 ducks ||= []
 ducks << RealDuck.new
 ducks << RubberDuck.new
 ducks << WoodDuck.new
+ducks << RobotsDuck.new
  
 ducks.each do |duck|
 puts "#{duck.class} #{duck.swiming}"
 puts "#{duck.class} #{duck.quacking}"
 puts "#{duck.class} #{duck.fly}"
+puts "#{duck.class} #{duck.eating}"
  
 end
